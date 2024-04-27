81ca9ff144fb9d918af5df249d8791faad3210eb
Terence Luk <88775938+terenceluk@users.noreply.github.com>
2024-04-27 14:01:22 -0400

    Create Add-Environment-Variables-App-Setting.ps1

---
+++ App Services/Add-Environment-Variables-App-Setting.ps1
@@ -0,0 +1,70 @@
+<#
+
+This script first retrieves all existing settings and stores them into a hash table. 
+Then it updates this hash table with new key-value pairs from the JSON. 
+Lastly, uses the hash table to update the app settings in the app service.
+
+More information about the environment variables can be found here: https://github.com/microsoft/sample-app-aoai-chatGPT
+
+#>
+
+# Log into Azure
+Connect-AzAccount
+
+# Define required variables for the App Service to be configured
+$subscriptionID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxx"
+$webAppRG = "dev-aisearch-rg"
+$webAppName = "dev-app"
+
+# Select the subscription with the App Service using the subscription ID
+Set-AzContext -Subscription $subscriptionID
+
+# Get the Web App object and store it
+$webApp = Get-AzWebApp -ResourceGroupName $webAppRG -Name $webAppName
+
+# Define JSON in code with the environment variables to be added
+$json = '
+[
+    {
+        "key": "UI_CHAT_LOGO",
+        "value": "https://storageaccount.blob.core.windows.net/images/Logo.png"
+    },
+    {
+        "key": "UI_CHAT_TITLE",
+        "value": "Start chatting with the bot"
+    },
+    {
+        "key": "UI_FAVICON",
+        "value": "https://storageaccount.blob.core.windows.net/images/favicon.ico"
+    },
+    {
+        "key": "UI_LOGO",
+        "value": "https://storageaccount.blob.core.windows.net/images/Logo.png"
+    },
+    {
+        "key": "UI_TITLE",
+        "value": "Contoso Company Co."
+    },
+    {
+        "key": "UI_CHAT_DESCRIPTION",
+        "value": "Use this bot to ask questions about anything!"
+    }
+]
+' | ConvertFrom-Json
+
+# Create a hash table for the application settings of the App Service
+$appSettingsHashTable = @{}
+
+# Populate the hash table with current App Service settings
+foreach($appSetting in $webApp.SiteConfig.AppSettings) {
+    $appSettingsHashTable[$appSetting.Name] = $appSetting.Value
+       
+}
+
+# Add additional settings from the JSON
+foreach($pair in $json) {
+    $appSettingsHashTable[$pair.key] = $pair.value
+}
+
+# Update the App Service with the hash table containing the new key-value pairs
+Set-AzWebApp -ResourceGroupName $webAppRG -Name $webAppName -AppSettings $appSettingsHashTable
